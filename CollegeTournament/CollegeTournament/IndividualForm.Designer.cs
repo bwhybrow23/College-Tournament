da951b90c87aa59fbbad215f0775f6292d83f6eb
Ben Whybrow <me@benwhybrow.com>
2021-01-12 10:47:09 +0000

    OneDrive sucks

---
+++ CollegeTournament/CollegeTournament/IndividualForm.Designer.cs
@@ -0,0 +1,217 @@
+﻿
+namespace CollegeTournament
+{
+    partial class IndividualForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_importData = new System.Windows.Forms.Button();
+            this.btn_exportData = new System.Windows.Forms.Button();
+            this.btn_switchToTeam = new System.Windows.Forms.Button();
+            this.btn_calculateWinner = new System.Windows.Forms.Button();
+            this.btn_resetScores = new System.Windows.Forms.Button();
+            this.lbl_teamScores = new System.Windows.Forms.Label();
+            this.lbl_individualNames = new System.Windows.Forms.Label();
+            this.txt_individualName_4 = new System.Windows.Forms.TextBox();
+            this.txt_individualName_3 = new System.Windows.Forms.TextBox();
+            this.txt_individualName_2 = new System.Windows.Forms.TextBox();
+            this.txt_individualName_1 = new System.Windows.Forms.TextBox();
+            this.lbl_title = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt_importData
+            // 
+            this.txt_importData.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_importData.Location = new System.Drawing.Point(469, 612);
+            this.txt_importData.Name = "txt_importData";
+            this.txt_importData.Size = new System.Drawing.Size(128, 62);
+            this.txt_importData.TabIndex = 43;
+            this.txt_importData.Text = "Import Data";
+            this.txt_importData.UseVisualStyleBackColor = true;
+            // 
+            // btn_exportData
+            // 
+            this.btn_exportData.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_exportData.Location = new System.Drawing.Point(319, 612);
+            this.btn_exportData.Name = "btn_exportData";
+            this.btn_exportData.Size = new System.Drawing.Size(128, 62);
+            this.btn_exportData.TabIndex = 42;
+            this.btn_exportData.Text = "Export Data";
+            this.btn_exportData.UseVisualStyleBackColor = true;
+            // 
+            // btn_switchToTeam
+            // 
+            this.btn_switchToTeam.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_switchToTeam.Location = new System.Drawing.Point(1091, 612);
+            this.btn_switchToTeam.Name = "btn_switchToTeam";
+            this.btn_switchToTeam.Size = new System.Drawing.Size(155, 62);
+            this.btn_switchToTeam.TabIndex = 41;
+            this.btn_switchToTeam.Text = "Team\r\nEvents";
+            this.btn_switchToTeam.UseVisualStyleBackColor = true;
+            this.btn_switchToTeam.Click += new System.EventHandler(this.btn_switchToTeam_Click);
+            // 
+            // btn_calculateWinner
+            // 
+            this.btn_calculateWinner.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_calculateWinner.Location = new System.Drawing.Point(169, 612);
+            this.btn_calculateWinner.Name = "btn_calculateWinner";
+            this.btn_calculateWinner.Size = new System.Drawing.Size(128, 62);
+            this.btn_calculateWinner.TabIndex = 40;
+            this.btn_calculateWinner.Text = "Calculate Winner";
+            this.btn_calculateWinner.UseVisualStyleBackColor = true;
+            // 
+            // btn_resetScores
+            // 
+            this.btn_resetScores.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_resetScores.Location = new System.Drawing.Point(19, 612);
+            this.btn_resetScores.Name = "btn_resetScores";
+            this.btn_resetScores.Size = new System.Drawing.Size(128, 62);
+            this.btn_resetScores.TabIndex = 39;
+            this.btn_resetScores.Text = "Reset Scores";
+            this.btn_resetScores.UseVisualStyleBackColor = true;
+            // 
+            // lbl_teamScores
+            // 
+            this.lbl_teamScores.AutoSize = true;
+            this.lbl_teamScores.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(157)))), ((int)(((byte)(195)))), ((int)(((byte)(230)))));
+            this.lbl_teamScores.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_teamScores.Location = new System.Drawing.Point(511, 103);
+            this.lbl_teamScores.Name = "lbl_teamScores";
+            this.lbl_teamScores.Size = new System.Drawing.Size(391, 39);
+            this.lbl_teamScores.TabIndex = 38;
+            this.lbl_teamScores.Text = "Scores (Heirachy of Winning)";
+            // 
+            // lbl_individualNames
+            // 
+            this.lbl_individualNames.AutoSize = true;
+            this.lbl_individualNames.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(157)))), ((int)(((byte)(195)))), ((int)(((byte)(230)))));
+            this.lbl_individualNames.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_individualNames.Location = new System.Drawing.Point(61, 103);
+            this.lbl_individualNames.Name = "lbl_individualNames";
+            this.lbl_individualNames.Size = new System.Drawing.Size(241, 39);
+            this.lbl_individualNames.TabIndex = 37;
+            this.lbl_individualNames.Text = "Individual Names";
+            // 
+            // txt_individualName_4
+            // 
+            this.txt_individualName_4.AcceptsTab = true;
+            this.txt_individualName_4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_individualName_4.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_individualName_4.Location = new System.Drawing.Point(63, 422);
+            this.txt_individualName_4.Name = "txt_individualName_4";
+            this.txt_individualName_4.Size = new System.Drawing.Size(392, 47);
+            this.txt_individualName_4.TabIndex = 36;
+            // 
+            // txt_individualName_3
+            // 
+            this.txt_individualName_3.AcceptsTab = true;
+            this.txt_individualName_3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_individualName_3.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_individualName_3.Location = new System.Drawing.Point(63, 332);
+            this.txt_individualName_3.Name = "txt_individualName_3";
+            this.txt_individualName_3.Size = new System.Drawing.Size(392, 47);
+            this.txt_individualName_3.TabIndex = 35;
+            // 
+            // txt_individualName_2
+            // 
+            this.txt_individualName_2.AcceptsTab = true;
+            this.txt_individualName_2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_individualName_2.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_individualName_2.Location = new System.Drawing.Point(63, 242);
+            this.txt_individualName_2.Name = "txt_individualName_2";
+            this.txt_individualName_2.Size = new System.Drawing.Size(392, 47);
+            this.txt_individualName_2.TabIndex = 34;
+            // 
+            // txt_individualName_1
+            // 
+            this.txt_individualName_1.AcceptsTab = true;
+            this.txt_individualName_1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_individualName_1.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_individualName_1.Location = new System.Drawing.Point(63, 152);
+            this.txt_individualName_1.Name = "txt_individualName_1";
+            this.txt_individualName_1.Size = new System.Drawing.Size(392, 47);
+            this.txt_individualName_1.TabIndex = 33;
+            // 
+            // lbl_title
+            // 
+            this.lbl_title.AutoSize = true;
+            this.lbl_title.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(85)))), ((int)(((byte)(151)))));
+            this.lbl_title.Font = new System.Drawing.Font("Calibri", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_title.Location = new System.Drawing.Point(420, 2);
+            this.lbl_title.Name = "lbl_title";
+            this.lbl_title.Size = new System.Drawing.Size(498, 42);
+            this.lbl_title.TabIndex = 32;
+            this.lbl_title.Text = "Scoring System - Individual Events";
+            this.lbl_title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // IndividualForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImage = global::CollegeTournament.Properties.Resources.UI_Design;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.txt_importData);
+            this.Controls.Add(this.btn_exportData);
+            this.Controls.Add(this.btn_switchToTeam);
+            this.Controls.Add(this.btn_calculateWinner);
+            this.Controls.Add(this.btn_resetScores);
+            this.Controls.Add(this.lbl_teamScores);
+            this.Controls.Add(this.lbl_individualNames);
+            this.Controls.Add(this.txt_individualName_4);
+            this.Controls.Add(this.txt_individualName_3);
+            this.Controls.Add(this.txt_individualName_2);
+            this.Controls.Add(this.txt_individualName_1);
+            this.Controls.Add(this.lbl_title);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "IndividualForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Scoring System";
+            this.Load += new System.EventHandler(this.IndividualForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button txt_importData;
+        private System.Windows.Forms.Button btn_exportData;
+        private System.Windows.Forms.Button btn_switchToTeam;
+        private System.Windows.Forms.Button btn_calculateWinner;
+        private System.Windows.Forms.Button btn_resetScores;
+        private System.Windows.Forms.Label lbl_teamScores;
+        private System.Windows.Forms.Label lbl_individualNames;
+        private System.Windows.Forms.TextBox txt_individualName_4;
+        private System.Windows.Forms.TextBox txt_individualName_3;
+        private System.Windows.Forms.TextBox txt_individualName_2;
+        private System.Windows.Forms.TextBox txt_individualName_1;
+        private System.Windows.Forms.Label lbl_title;
+    }
+}
---
+++ CollegeTournament/CollegeTournament/IndividualForm.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CollegeTournament
+{
+    public partial class IndividualForm : Form
+    {
+        DomainUpDown[] ctr_individual;
+        const int NUMBEROFBOXES = 20;
+        teamForm mainForm;
+
+        public IndividualForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_resetScores_Click(object sender, EventArgs e)
+        {
+            // Message pop-up to confirm
+            DialogResult dialogresult = MessageBox.Show("Are you sure you would like to reset all scores?", "Reset Scores", MessageBoxButtons.YesNo);
+            if (dialogresult == DialogResult.Yes)
+            {
+                //If Yes is clicked on the pop-up, reset the scores                
+                for (int i = 0; i < NUMBEROFBOXES; i++)
+                {
+                    ctr_individual[i].SelectedIndex = 0;
+                }
+            }
+            else if (dialogresult == DialogResult.No)
+            {
+                //Return nothing as the event has been cancelled
+                return;
+            }
+        }
+
+        private void IndividualForm_Load(object sender, EventArgs e)
+        {
+            //Declaration of values used
+            int i, j;
+            int yPos;
+
+            ctr_individual = new DomainUpDown[NUMBEROFBOXES];
+
+            //For loop to create all boxes
+            for (i = 0; i < NUMBEROFBOXES; i++)
+            {
+                yPos = 147 + (i / 5 * 90);
+                ctr_individual[i] = new DomainUpDown();
+                ctr_individual[i].Location = new Point(527 + (90 * (i % 5)), yPos);
+                ctr_individual[i].Width = 55;
+                ctr_individual[i].Font = new Font("Calibri", 24);
+
+                for (j = 0; j <= 20; j++)
+                {
+                    ctr_individual[i].Items.Add(j.ToString());
+                }
+
+                ctr_individual[i].SelectedIndex = 0;
+                ctr_individual[i].Parent = this;
+            }
+        }
+
+        private void btn_calculateWinner_Click(object sender, EventArgs e)
+        {
+            // Input Validation
+            if (txt_individualName_1.Text == "" || txt_individualName_2.Text == "" || txt_individualName_3.Text == "" || txt_individualName_4.Text == "")
+            {
+                MessageBox.Show("One of the individual names is invalid, please check them.");
+                return;
+            }
+
+            //Convert individual names to Array
+            string[] individualNames = { txt_individualName_1.Text, txt_individualName_2.Text, txt_individualName_3.Text, txt_individualName_4.Text };
+
+            //Setup array for scores
+            int[] scores = { 0, 0, 0, 0, 0 };
+            int j = 0;
+
+            //For loop for every value
+            for (int i = 0; i < NUMBEROFBOXES; i++)
+            {
+                //Every 5 boxes, move to next row (next individual)
+                if (i % 5 == 0)
+                {
+                    j++;
+                }
+
+                //Check if 0
+                if (ctr_individual[i].SelectedIndex == 0)
+                {
+                    MessageBox.Show("None of the values can be 0");
+                    return;
+                }
+
+                //Input Validation
+                try
+                {
+                    Int32.Parse(ctr_individual[i].SelectedIndex.ToString());
+                }
+                catch
+                {
+                    MessageBox.Show("Some scores are not valid. Change the scores and try again.");
+                    return;
+                }
+
+                //Add value of box to individual score
+                scores[j] += ctr_individual[i].SelectedIndex;
+            }
+
+            //Calculate maximum value and position of said value
+            int max = scores.Max();
+            int mPos = Array.IndexOf(scores, max);
+
+            //Use the array to gather the winning individual
+            string winningIndividual = individualNames[mPos - 1];
+
+            //Output the winning individual and return
+            MessageBox.Show("The person " + winningIndividual + " won with a score of " + max.ToString());
+            return;
+
+        }
+
+        private void btn_exportData_Click(object sender, EventArgs e)
+        {
+
+            //GATHER DATA
+
+            // Input Validation
+            if (txt_individualName_1.Text == "" || txt_individualName_2.Text == "" || txt_individualName_3.Text == "" || txt_individualName_4.Text == "")
+            {
+                MessageBox.Show("One of the individual names is invalid, please check them.");
+                return;
+            }
+
+            //Convert individual names to Array
+            string[] individualNames = { txt_individualName_1.Text, txt_individualName_2.Text, txt_individualName_3.Text, txt_individualName_4.Text };
+
+            //Setup array for scores
+            string[] scores = { "", "", "", "", "" };
+            int j = 0;
+
+            //For loop for every value
+            for (int i = 0; i < NUMBEROFBOXES; i++)
+            {
+                //Every 5 boxes, move to next row (next individual)
+                if (i % 5 == 0)
+                {
+                    j++;
+                }
+
+                //Check if 0
+                if (ctr_individual[i].SelectedIndex == 0)
+                {
+                    MessageBox.Show("Cannot export any scores that contain 0");
+                    return;
+                }
+
+                //Input Validation
+                try
+                {
+                    Int32.Parse(ctr_individual[i].SelectedIndex.ToString());
+                }
+                catch
+                {
+                    MessageBox.Show("Some scores are not valid. Change the scores and try again.");
+                    return;
+                }
+
+                //Add value of box to individual score
+                scores[j] = scores[j] + " " + ctr_individual[i].SelectedIndex.ToString();
+            }
+
+            // Open output file stream
+            try
+            {
+                FileStream myFile = new FileStream("individual-export.txt", FileMode.OpenOrCreate);
+                StreamWriter sw = new StreamWriter(myFile);
+
+                //Save each individual name and scores to each line
+                sw.WriteLine(individualNames[0]);
+                sw.WriteLine(scores[1]);
+                sw.WriteLine(individualNames[1]);
+                sw.WriteLine(scores[2]);
+                sw.WriteLine(individualNames[2]);
+                sw.WriteLine(scores[3]);
+                sw.WriteLine(individualNames[3]);
+                sw.WriteLine(scores[4]);
+                sw.Close();
+            }
+            catch
+            {
+                // Something went wrong!
+                MessageBox.Show("Could not save to file - check file permissions");
+                return;
+            }
+
+            // Success!
+            MessageBox.Show("Data successfully saved to file!");
+        }
+
+        private void txt_importData_Click(object sender, EventArgs e)
+        {
+
+            //Values to input data onto
+            string individualName_1, individualName_2, individualName_3, individualName_4;
+            string score_1, score_2, score_3, score_4;
+
+            //Attempt to fetch data from file
+            try
+            {
+                FileStream myFile = new FileStream("individual-export.txt", FileMode.Open);
+                StreamReader sr = new StreamReader(myFile);
+
+                //Read each line for the values
+                individualName_1 = sr.ReadLine();
+                score_1 = sr.ReadLine();
+                individualName_2 = sr.ReadLine();
+                score_2 = sr.ReadLine();
+                individualName_3 = sr.ReadLine();
+                score_3 = sr.ReadLine();
+                individualName_4 = sr.ReadLine();
+                score_4 = sr.ReadLine();
+                sr.Close();
+            }
+            catch
+            {
+                // Something went wrong!
+                MessageBox.Show("Could not load data from file - file is missing or corrupt");
+                return;
+            }
+
+            //Convert scores into array
+            string[] scores_1 = score_1.Split(' ');
+            string[] scores_2 = score_2.Split(' ');
+            string[] scores_3 = score_3.Split(' ');
+            string[] scores_4 = score_4.Split(' ');
+
+            // Input data to form
+            // Individual Names
+            txt_individualName_1.Text = individualName_1;
+            txt_individualName_2.Text = individualName_2;
+            txt_individualName_3.Text = individualName_3;
+            txt_individualName_4.Text = individualName_4;
+
+            //Scores
+            int pos = 0;
+
+            //Scores-1
+            foreach (string number in scores_1)
+            {
+                if (number == "")
+                {
+                    //Do nothing cuz this is an empty string :)
+                }
+                else
+                {
+                    ctr_individual[pos].SelectedIndex = Int32.Parse(number);
+                    pos++;
+                }
+            }
+            //Scores-2
+            foreach (string number in scores_2)
+            {
+                if (number == "")
+                {
+                    //Do nothing cuz this is an empty string :)
+                }
+                else
+                {
+                    ctr_individual[pos].SelectedIndex = Int32.Parse(number);
+                    pos++;
+                }
+            }
+            //Scores-3
+            foreach (string number in scores_3)
+            {
+                if (number == "")
+                {
+                    //Do nothing cuz this is an empty string :)
+                }
+                else
+                {
+                    ctr_individual[pos].SelectedIndex = Int32.Parse(number);
+                    pos++;
+                }
+            }
+            //Scores-4
+            foreach (string number in scores_4)
+            {
+                if (number == "")
+                {
+                    //Do nothing cuz this is an empty string :)
+                }
+                else
+                {
+                    ctr_individual[pos].SelectedIndex = Int32.Parse(number);
+                    pos++;
+                }
+            }
+
+            // Successfully read data
+            MessageBox.Show("Successfully read data from file");
+
+        }
+
+        private void btn_switchToTeam_Click(object sender, EventArgs e)
+        {
+            // Message pop-up to confirm
+            DialogResult dialogresult = MessageBox.Show("Are you sure you would like to switch to Team Scores? This will erase all data so make sure to export the data.", "Switch to Team Scores", MessageBoxButtons.YesNo);
+            if (dialogresult == DialogResult.Yes)
+            {
+                //If yes, switch to individual form
+                mainForm.Show();
+                this.Hide();
+            }
+            else if (dialogresult == DialogResult.No)
+            {
+                //Return nothing as the event has been cancelled
+                return;
+            }
+        }
+    }
+    }
